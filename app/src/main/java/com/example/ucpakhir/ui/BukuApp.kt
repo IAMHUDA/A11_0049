26e84d216fe68ce28ba86b4e8bdf471c34523708
IAMHUDA <hudamiftahul362@gmail.com>
2025-01-28 12:38:30 +0700

    add app

---
+++ app/src/main/java/com/example/ucpakhir/ui/BukuApp.kt
@@ -0,0 +1,36 @@
+package com.example.ucpakhir.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
+import com.example.ucpakhir.BukuApplications
+import com.example.ucpakhir.di.AppContainerBuku
+import com.example.ucpakhir.ui.navigation.PengelolaHalaman
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun  BukuApp(
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    // Get the application instance
+    val appContainerBuku = (LocalContext.current.applicationContext as BukuApplications).containerBuku
+    Scaffold(
+//        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        //topBar = {TopAppBar(scrollBehavior = scrollBehavior)},
+    ){
+        Surface (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ){
+            PengelolaHalaman(appContainerBuku = appContainerBuku)
+        }
+    }
+}
